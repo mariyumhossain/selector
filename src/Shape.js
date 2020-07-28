1fa6b3ab2c658081919fe7b1968e1d31c2b03d4e
Mariyum Hossain <mariyumhossain@Mariyums-MacBook-Air.local>
2020-07-28 17:13:32 -0400

    working selector

---
+++ src/Shape.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Shape = (props) => {
+    const shape = props.shape
+    const selectShape = props.selectShape
+    return (
+        <div className={props.shape} onClick={() => selectShape(shape)} />
+    )
+}
+
+export default Shape
